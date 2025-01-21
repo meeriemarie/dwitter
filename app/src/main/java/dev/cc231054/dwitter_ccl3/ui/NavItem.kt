e61ad6f86b65ff8f2555465cd6d7f52399beeeaf
cc231073 <cc231073@fhstp.ac.at>
2025-01-21 08:08:37 +0100

    Navigation UI

--- app/src/main/java/dev/cc231054/dwitter_ccl3/ui/NavItem.kt
+++ app/src/main/java/dev/cc231054/dwitter_ccl3/ui/NavItem.kt
@@ -4,10 +4,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -25,24 +27,36 @@ fun BottomNavigationBar(navController: NavController){
     val activeRoute = navController.currentBackStackEntryAsState().value?.destination?.route?:Screens.Home.name
     val activeSreen = Screens.valueOf(activeRoute)
 
-    NavigationBar {
+    NavigationBar{
         NavigationBarItem(
             selected = activeSreen == Screens.Home,
             onClick = {navController.navigate(Screens.Home.name)},
-            icon = { Icon(imageVector = Icons.Filled.Home, contentDescription = "Home") },
-            label = { Text("Home") }
+            icon = {
+                Icon(
+                    imageVector = if (activeSreen != Screens.Home) Icons.Outlined.Home else Icons.Filled.Home,
+                    contentDescription = "Home",
+                )
+            },
         )
         NavigationBarItem(
             selected = activeSreen == Screens.Search,
             onClick = {navController.navigate(Screens.Search.name)},
-            icon = { Icon(imageVector = Icons.Filled.Search, contentDescription = "Search") },
-            label = { Text("Search") }
+            icon = {
+                Icon(
+                    imageVector = if (activeSreen != Screens.Search) Icons.Outlined.Search else Icons.Filled.Search,
+                    contentDescription = "Search",
+                )
+            },
         )
         NavigationBarItem(
             selected = activeSreen == Screens.Profile,
             onClick = {navController.navigate(Screens.Profile.name)},
-            icon = { Icon(imageVector = Icons.Filled.Person, contentDescription = "Profile") },
-            label = { Text("Profile") }
+            icon = {
+                Icon(
+                    imageVector = if (activeSreen != Screens.Profile) Icons.Outlined.Person else Icons.Filled.Person,
+                    contentDescription = "Profile",
+                )
+            },
         )
     }
 }
